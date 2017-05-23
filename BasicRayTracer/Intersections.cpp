b9b7c775eede71b93f2a8cbacda653f2d9683cbd
John T. Alexander <jalexander.qed@gmail.com>
2017-05-23 12:09:35 -0700

    Adapted polyintersect to not use glm

--- BasicRayTracer/Intersections.cpp
+++ BasicRayTracer/Intersections.cpp
@@ -80,7 +80,7 @@ MaterialIO interpolateMaterials(const glm::vec3& p1, const glm::vec3& p2, const 
 	resMat.ktran = material3->ktran * weights.x +
 		material1->ktran * weights.y +
 		material2->ktran * weights.z;
-	
+
 	return resMat;
 }
 
@@ -150,6 +150,30 @@ IntersectionPoint intersectSphere(const glm::vec3& vec, const glm::vec3& origin,
 	return point;
 }
 
+inline void cross(const glm::vec3& v1, float* v2, float* res) {
+	res[0] = v1[1] * v2[2] - v1[2] * v2[1];
+	res[1] = v1[2] * v2[0] - v1[0] * v2[2];
+	res[2] = v1[0] * v2[1] - v1[1] * v2[0];
+}
+
+inline void cross(float* v1, float* v2, float* res) {
+	res[0] = v1[1] * v2[2] - v1[2] * v2[1];
+	res[1] = v1[2] * v2[0] - v1[0] * v2[2];
+	res[2] = v1[0] * v2[1] - v1[1] * v2[0];
+}
+
+inline float dot(float* v1, float* v2) {
+	return v1[0] * v2[0] +
+		v1[1] * v2[1] +
+		v1[2] * v2[2];
+}
+
+inline float dot(const glm::vec3& v1, float* v2) {
+	return v1[0] * v2[0] +
+		v1[1] * v2[1] +
+		v1[2] * v2[2];
+}
+
 PolyIntersectionPoint intersectPoly(const glm::vec3& vec, const glm::vec3& origin, PolygonIO* poly) {
 #ifdef DEBUG
 	if (poly->numVertices != 3) {
@@ -159,28 +183,47 @@ PolyIntersectionPoint intersectPoly(const glm::vec3& vec, const glm::vec3& origi
 #endif // DEBUG
 
 	PolyIntersectionPoint point;
+	/*
 	glm::vec3 vert0(poly->vert[0].pos[0], poly->vert[0].pos[1], poly->vert[0].pos[2]);
 	glm::vec3 vert1(poly->vert[1].pos[0], poly->vert[1].pos[1], poly->vert[1].pos[2]);
 	glm::vec3 vert2(poly->vert[2].pos[0], poly->vert[2].pos[1], poly->vert[2].pos[2]);
 
 	glm::vec3 edge0 = vert1 - vert0;
 	glm::vec3 edge1 = vert2 - vert0;
-	glm::vec3 p = glm::cross(vec, edge1);
+	glm::vec3 p = glm::cross(vec, edge1);*/
 
-	float determinant = glm::dot(edge0, p);
+	float edge0[3] = {
+		poly->vert[1].pos[0] - poly->vert[0].pos[0],
+		poly->vert[1].pos[1] - poly->vert[0].pos[1],
+		poly->vert[1].pos[2] - poly->vert[0].pos[2]
+	};
+	float edge1[3] = {
+		poly->vert[2].pos[0] - poly->vert[0].pos[0],
+		poly->vert[2].pos[1] - poly->vert[0].pos[1],
+		poly->vert[2].pos[2] - poly->vert[0].pos[2]
+	};
+	float p[3];
+	cross(vec, edge1, p);
+
+	float determinant = dot(edge0, p);
 	if (determinant > -EPSILON && determinant < EPSILON) return point;
 	float invDeterminant = 1 / determinant;
 
-	glm::vec3 vertToOrigin = origin - vert0;
+	float vertToOrigin[3] = {
+		origin[0] - poly->vert[0].pos[0],
+		origin[1] - poly->vert[0].pos[1],
+		origin[2] - poly->vert[0].pos[2]
+	};
 
-	float u = glm::dot(vertToOrigin, p) * invDeterminant;
+	float u = dot(vertToOrigin, p) * invDeterminant;
 	if (u < 0 || u > 1) return point;
 
-	glm::vec3 q = glm::cross(vertToOrigin, edge0);
-	float v = glm::dot(q, vec) * invDeterminant;
+	float q[3];
+	cross(vertToOrigin, edge0, q);
+	float v = dot(vec, q) * invDeterminant;
 	if (v < 0 || u + v > 1) return point;
 
-	float t = glm::dot(edge1, q) * invDeterminant;
+	float t = dot(edge1, q) * invDeterminant;
 
 	if (t > EPSILON) {
 		point = { origin + t * vec, poly };
@@ -198,7 +241,7 @@ IntersectionPoint intersectPolySet(const glm::vec3& vec, const glm::vec3& origin
 	if (polySet->type != POLYSET_TRI_MESH) {
 		cerr << "ERROR: Polyset type is not POLYSET_TRI_MESH" << endl;
 		exit(1);
-	}
+}
 #endif // DEBUG
 
 	PolygonIO* poly = polySet->poly;
